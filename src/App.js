b8595349944537ec94f5a47c2f7980ed52f29199
Kirby <110064066+McKennie8@users.noreply.github.com>
2022-12-04 19:00:11 -0500

    3. game2 stuck

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import React from "react";
-//Components and pages
-import Home from "./pages/Home";
-import Nav from "./components/Nav";
-//Styles
-import GlobalStyles from "./components/GlobalStyles";
-//Router
-import { Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { loadGames } from "./actions/gamesAction";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadGames());
+  });
   return (
     <div className="App">
-      <GlobalStyles />
-      <Nav />
-      <Route path={["/game/:id", "/"]}>
-        <Home />
-      </Route>
+      <h1>Hello Plibs</h1>
     </div>
   );
 }
