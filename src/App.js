1e9f7adac9aa3271f92dffe7a6fe52363eb2e2d5
Kirby <110064066+McKennie8@users.noreply.github.com>
2022-12-05 15:13:05 -0500

    Home again

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React, { useEffect } from "react";
-<<<<<<< HEAD
 //Components and Pages
 import Home from "./pages/Home";
-=======
-import { useDispatch } from "react-redux";
-import { loadGames } from "./actions/gamesAction";
->>>>>>> b8595349944537ec94f5a47c2f7980ed52f29199
 
 function App() {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(loadGames());
-  });
   return (
     <div className="App">
       <h1>Hello Plibs</h1>
-<<<<<<< HEAD
       <Home />
-=======
->>>>>>> b8595349944537ec94f5a47c2f7980ed52f29199
     </div>
   );
 }
