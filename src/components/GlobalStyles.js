1fc134a2d59eb683ade0bba8e68b57deeceeb1a5
Kirby <110064066+McKennie8@users.noreply.github.com>
2022-12-05 20:36:08 -0500

    9. Global Styles

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -8,7 +8,37 @@ const GlobalStyles = createGlobalStyle`
     }
     html{
         &::-webkit-scrollbar{
-            width: 0.5rem;
+            width: 1rem;
+            
         }
+        &::-webkit-scrollbar-thumb{
+            background-color: #5f6cff;
+        }
+    body{
+        font-family: 'Montserrat', sans-serif;
+        width: 100%;
+    }
+    h2{
+        font-size: 3rem;
+        font-family: 'Abril Fatface', cursive;
+        font-weight: lighter;
+        color: #5f6cff;
+    }
+    h3{
+        font-size: 1.3rem;
+        color: #333;
+        padding: 1.5rem;
+    }
+    p{
+        font-size: 1.2rem;
+        line-height: 200%;
+        color: #696969;
+    }
+    a{
+        text-decoration: none;
+        color: #333;
+    }
     }
 `;
+
+export default GlobalStyles;
